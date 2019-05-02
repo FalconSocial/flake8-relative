fb1822c39979548e56657aef66b6a3226092d1ed
Tue Haulund <tue.haulund@gmail.com>
2019-05-02 21:03:36 +0200

    Added initial draft implementation

---
+++ flake8_relative.py
@@ -0,0 +1,29 @@
+"""Flake8 plugin checking for relative imports."""
+import ast
+
+R100 = 'R100: Found relative import'
+
+
+class RelativeImportFinder(ast.NodeVisitor):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.relative_imports = []
+
+    def visit_ImportFrom(self, node):
+        if node.level > 0:
+            self.relative_imports.append(node)
+
+
+class RelativeImportChecker:
+    name = 'flake8-relative'
+
+    def __init__(self, tree):
+        self.tree = tree
+
+    def run(self):
+        visitor = RelativeImportFinder()
+        visitor.visit(self.tree)
+
+        for import_node in visitor.relative_imports:
+            yield (import_node.lineno, import_node.col_offset, R100, type(self))
